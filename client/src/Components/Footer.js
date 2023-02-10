2ff49c73245d02d3b29c60c60612b0989a5a6281
shivammalviya205 <86865439+shivammalviya205@users.noreply.github.com>
2023-02-11 01:35:42 +0530

    about us and Footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,19 +1,149 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <div>
-            <footer className="bg-dark text-light text-center text-lg-start">
-                {/* Copyright */}
-                <div className="text-center p-3" >
-                    © {new Date().getFullYear()} Copyright:
-                    <Link className="text-light" to="/">Praedico Global</Link>
-                </div>
-                {/* Copyright */}
-            </footer>
-        </div>
-    )
+  return (
+    <footer class="bg-dark text-center text-white">
+      <div class="container p-4">
+        <section class="mb-4">
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i class="fab fa-facebook-f"></i>
+          </a>
+
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i class="fab fa-twitter"></i>
+          </a>
+
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i class="fab fa-google"></i>
+          </a>
+
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i class="fab fa-instagram"></i>
+          </a>
+
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href="#!"
+            role="button"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+        </section>
+
+        <section class="mb-4">
+          <p>
+            Praedico Global Research Private Limited is a private company which
+            is founded on 09 April, 2018. We deals in Stock Market Training,
+            Stock Marketing Predictive Softwares, Robotic Stock Trading, Global
+            Equity Research, Portfolio Designing, Financial Literacy and Stock
+            Market Research Using Deep Learning.
+          </p>
+        </section>
+
+        <section class="">
+          <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Useful Links</h5>
+
+              <ul class="list-unstyled mb-0">
+                <li>
+                  <a href="#!" class="text-white text-decoration-none">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white text-decoration-none">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white text-decoration-none">
+                    Career
+                  </a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white text-decoration-none">
+                    Blogs
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Our services</h5>
+
+              <ul class="list-unstyled mb-0">
+                <li className="mb-1">
+                  <a href="#!" class="text-white text-decoration-none">
+                    Web Design{" "}
+                  </a>
+                </li>
+                <li className="mb-1">
+                  <a href="#!" class="text-white text-decoration-none">
+                    Web Development
+                  </a>
+                </li>
+                <li className="mb-1" >
+                  <a href="#!" className="text-white text-decoration-none" >
+                    Product Management
+                  </a>
+                </li>
+                <li className="mb-1">
+                  <a href="#!" class="text-white text-decoration-none">
+                    Marketing
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 class="text-uppercase">Conatct Us</h5>
+              <p mt-5>HEAD OFFICE : Udyog Vihar,<br/>
+              Phase 4, Gurgaon</p>
+              <p mt-5>
+              DATA CENTER : First Floor, Garima Arcade,<br/> Gwalior
+              </p>
+              
+
+              
+            </div>
+
+
+          </div>
+        </section>
+      </div>
+
+      <div class="text-center p-3">
+        © 2023 Copyright: 
+        <Link class="text-decoration-none" to="/"> praedicoglobal.com</Link>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/pages/User/AboutUs/About.js
+++ client/src/pages/User/AboutUs/About.js
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import './About.css'
+import imgmf from '././mutualfund.jpg'
 function About() {
   return (
-    <div>
-      About
+    <div class="about-area">
+    <div class="container" >
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="about-img">
+                    <img src={imgmf} alt=""/>
+                </div>
+            </div>
+            <div class="col-lg-6 col-lg-offset-1">
+                <div class="about-text">
+                    <h2>About Us</h2>
+                    <p>The most important step in picking the best mutual funds is to focus on getting the mutual funds that fit into your needs. Once that is done, the second step is to evaluate alternate options. You need to create a mutual fund portfolio that has the right mix of equity funds, debt funds, liquid funds, variable funds etc. This mix needs to be tweaked consistently based on the external and internal stimuli</p>
+                    <a href="/">Check out our Product</a>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   )
 }
 
