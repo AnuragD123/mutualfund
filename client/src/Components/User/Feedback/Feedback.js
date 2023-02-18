2f38e5b4ba2154f0eb1957a0899999635e266ae1
Anurag Dubey <anuragdubey859@gmail.com>
2023-02-18 14:04:24 +0530

    Feedback Cards on Home page  Redesigned with dynamic star rating

--- client/src/Components/User/Feedback/Feedback.js
+++ client/src/Components/User/Feedback/Feedback.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import Rating from '@mui/material/Rating';
+
 
 function Feedback() {
 
@@ -9,13 +11,16 @@ function Feedback() {
   const fetchData = async () => {
     const responceFetch = await fetch('http://localhost:1337/api/user/fetchfeedbackdatauser');
     const resJSON = await responceFetch.json();
-    console.log(resJSON);
+
+    // console.log(resJSON);
+
     setData(resJSON.data)
   }
 
   useEffect(() => {
     fetchData();
   }, []);
+
 
   return (
 
@@ -42,28 +47,17 @@ function Feedback() {
                 <div className="card">
                   <div className="card-body py-4 mt-2">
                     <div className="d-flex justify-content-center mb-4">
-                      <img alt='userimage' src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp"
-                        className="rounded-circle shadow-1-strong" width="100" height="100" />
+                      {/* <img alt='userimage' src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(10).webp"
+                        className="rounded-circle shadow-1-strong" width="100" height="100" /> */}
+                        <div className="icon border border-5 border-success rounded-circle text-success " style={{width:"120px",height:"120px",display:"flex", alignItems:"center", justifyContent:"center"}}>
+                          <i class="fa fa-user display-2 fa-2xl " aria-hidden="true"></i>
+
+                        </div>
                     </div>
                     <h5 className="font-weight-bold">{d.name}</h5>
                     {/* <h6 className="font-weight-bold my-3">Founder at ET Company</h6> */}
-                    <ul className="list-unstyled d-flex justify-content-center">
-                      <li>
-                        <i className="fas fa-star fa-sm text-info"></i>
-                      </li>
-                      <li>
-                        <i className="fas fa-star fa-sm text-info"></i>
-                      </li>
-                      <li>
-                        <i className="fas fa-star fa-sm text-info"></i>
-                      </li>
-                      <li>
-                        <i className="fas fa-star fa-sm text-info"></i>
-                      </li>
-                      <li>
-                        <i className="fas fa-star-half-alt fa-sm text-info"></i>
-                      </li>
-                    </ul>
+                    <Rating name="read-only" value={d.rating} readOnly />
+          
                     <p className="mb-2">
                       <i className="fas fa-quote-left pe-2"></i>{d.message}
                     </p>
