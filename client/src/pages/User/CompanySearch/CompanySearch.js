8b2dcfb8901a62124f727f256b82d4d3cb2717fe
Saksham <sakshamsharma8982@gmail.com>
2023-04-30 16:30:59 +0530

    company finder issue resolved

--- client/src/pages/User/CompanySearch/CompanySearch.js
+++ client/src/pages/User/CompanySearch/CompanySearch.js
@@ -3,32 +3,41 @@ import data from "./CompanyData.json";
 import "./CompanySearch.css";
 
 function CompanySearch() {
+  //console.log(data);
   const [companyName, setCompanyName] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Form submitted");
-    const { CMP_Rs, P_E_Ratio, Mar_Cap_Rs_Cr, Div_Yld_Percentage } = event.target.elements;
+    const { CMP_Rs , P_E_Ratio, Mar_Cap_Rs_Cr, Div_Yld_Percentage  } = event.target.elements;
 
-    if (data && data.companies && data.companies.length > 0) {
-      const company = data.companies.find(
-        (c) =>
-          c.CMP_Rs === Number(CMP_Rs.value) &&
-          c.P_E_Ratio === Number(P_E_Ratio.value) &&
-          c.Mar_Cap_Rs_Cr === Number(Mar_Cap_Rs_Cr.value) &&
-          c.Div_Yld_Percentage === Number(Div_Yld_Percentage.value)
-      );
+    console.log("CMP_Rs:", CMP_Rs);
+    console.log("P_E_Ratio:", P_E_Ratio);
+    console.log("Mar_Cap_Rs_Cr:", Mar_Cap_Rs_Cr);
+    console.log("Div_Yld_Percentage:", Div_Yld_Percentage); 
 
-      if (company) {
-        setCompanyName(company.name);
-        console.log(company.name);
+    //console.log(CMP_Rs.value, P_E_Ratio.value, Mar_Cap_Rs_Cr.value, Div_Yld_Percentage.value);
+
+    if (CMP_Rs /* && P_E_Ratio && Mar_Cap_Rs_Cr && Div_Yld_Percentage */) {
+      if (data && data.companies && data.companies.length > 0) {
+        const companies = data.companies.filter(c => c.CMP_Rs === CMP_Rs.value &&
+          c.P_E_Ratio === P_E_Ratio.value &&
+          c.Mar_Cap_Rs_Cr === Mar_Cap_Rs_Cr.value &&
+          c.Div_Yld_Percentage === Div_Yld_Percentage.value)
+
+        if (companies.length > 0) {
+          const names = companies.map((c) => c.name).join(", ");
+          setCompanyName(names);
+        } else {
+          setCompanyName("Company not found");
+        }
       } else {
-        setCompanyName("Company not found");
+        console.error("Error: data or data.companies is undefined or empty.");
       }
-    } else {
-      console.error("Error: data or data.companies is undefined or empty.");
     }
   };
+
+  
+  
   //console.log(data);
   
 
@@ -40,7 +49,7 @@ function CompanySearch() {
           <label htmlFor="CMP_Rs">CMP_Rs:</label>
           <input type="number" name="CMP_Rs" id="CMP_Rs" />
         </div>
-        {/* <div className="form-group">
+        <div className="form-group">
           <label htmlFor="P_E_Ratio">P_E_Ratio:</label>
           <input type="number" name="P_E_Ratio" id="P_E_Ratio" />
         </div>
@@ -51,7 +60,7 @@ function CompanySearch() {
         <div className="form-group">
           <label htmlFor="Div_Yld_Percentage">Div_Yld_Percentage:</label>
           <input type="number" name="Div_Yld_Percentage" id="Div_Yld_Percentage" />
-        </div> */}
+        </div>
         <button type="submit">Find Company</button>
       </form>
       <p>{companyName}</p>
